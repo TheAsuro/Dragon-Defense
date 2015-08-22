87201e4539c97db0b826b5d0f48196bb5c3cc49a
TheAsuro <theasuro@gmail.com>
2015-08-23 01:23:48 +0200

    HP Bar

---
+++ Assets/Code/UI/HealthBar.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour
+{
+    private RectTransform RT { get { return GetComponent<RectTransform>(); } }
+
+    void Update()
+    {
+        RT.anchorMax = new Vector2(WorldObjects.Player.Player.Current.Health / 100f, RT.anchorMax.y);
+    }
+}
