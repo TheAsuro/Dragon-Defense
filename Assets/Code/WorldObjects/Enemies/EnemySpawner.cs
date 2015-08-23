e8136514ce41a58a55ec52368838a6f5ea85f715
TheAsuro <theasuro@gmail.com>
2015-08-24 00:15:01 +0200

    Removed builds from github

--- Assets/Code/WorldObjects/Enemies/EnemySpawner.cs
+++ Assets/Code/WorldObjects/Enemies/EnemySpawner.cs
@@ -6,98 +6,19 @@ namespace WorldObjects.Enemies
 {
     public class EnemySpawner : MonoBehaviour
     {
-        private static List<EnemySpawner> allSpawners = new List<EnemySpawner>();
-        public static List<EnemySpawner> AllSpawners { get { return new List<EnemySpawner>(allSpawners); } }
-
-        // Lists so unity can serialize
-        [SerializeField]
-        private List<GameObject> spawnEnemies = new List<GameObject>();
-        [SerializeField]
-        private List<int> spawnEnemyCount = new List<int>();
-        [SerializeField]
-        private List<float> spawnEnemyTimes = new List<float>();
-        [SerializeField]
-        private List<bool> didSpawnEnemies = new List<bool>();
-
-        private float startTime;
-
-        public int Count { get { return spawnEnemies.Count; } }
+        private Dictionary<float, SpawnInfo> spawnData = new Dictionary<float,SpawnInfo>();
 
         void Start()
         {
-            allSpawners.Add(this);
-        }
-
-        void OnDestroy()
-        {
-            allSpawners.Remove(this);
-        }
-
-        public void StartSpawning()
-        {
-            startTime = Time.time;
-        }
-
-        public void AddEntry(GameObject enemy, int count, float time)
-        {
-            spawnEnemies.Add(enemy);
-            spawnEnemyCount.Add(count);
-            spawnEnemyTimes.Add(time);
-            didSpawnEnemies.Add(false);
-        }
-
-        public void RemoveEntryAt(int index)
-        {
-            spawnEnemies.RemoveAt(index);
-            spawnEnemyCount.RemoveAt(index);
-            spawnEnemyTimes.RemoveAt(index);
-            didSpawnEnemies.RemoveAt(index);
-        }
-
-        public GameObject GetEnemyAt(int index)
-        {
-            return spawnEnemies[index];
-        }
-
-        public void SetEnemyAt(int index, GameObject enemy)
-        {
-            spawnEnemies[index] = enemy;
-        }
-
-        public int GetCountAt(int index)
-        {
-            return spawnEnemyCount[index];
-        }
-
-        public void SetCountAt(int index, int count)
-        {
-            spawnEnemyCount[index] = count;
-        }
-
-        public float GetTimeAt(int index)
-        {
-            return spawnEnemyTimes[index];
-        }
-
-        public void SetTimeAt(int index, float time)
-        {
-            spawnEnemyTimes[index] = time;
+            SpawnInfo test = new SpawnInfo("0|1|AggressiveEnemy1");
+            spawnData.Add(test.)
         }
 
         void Update()
         {
-            float spawnTime = Time.time - startTime;
-
-            for (int i = 0; i < spawnEnemies.Count; i++)
+            foreach (KeyValuePair<float, SpawnInfo> pair in spawnData)
             {
-                if (!didSpawnEnemies[i] && spawnTime >= spawnEnemyTimes[i])
-                {
-                    didSpawnEnemies[i] = true;
-                    for (int spawnCount = 0; spawnCount < spawnEnemyCount[i]; spawnCount++)
-                    {
-                        Spawn(spawnEnemies[i]);
-                    }
-                }
+
             }
         }
 
@@ -107,32 +28,26 @@ namespace WorldObjects.Enemies
         }
     }
 
-    [CustomEditor(typeof(EnemySpawner))]
-    public class EnemySpawnerEditor : Editor
+    public struct SpawnInfo
     {
-        public override void OnInspectorGUI()
+        public SpawnInfo(string text)
         {
-            EnemySpawner spawner = (EnemySpawner)target;
+            string[] data = text.Split('|');
+            if (data.Length < 3)
+                throw new System.Exception();
 
-            EditorGUILayout.BeginVertical();
-            EditorGUILayout.LabelField("Type | Count | Time");
-            for (int i = 0; i < spawner.Count; i++)
-            {
-                EditorGUILayout.BeginHorizontal();
-                spawner.SetEnemyAt(i, (GameObject)EditorGUILayout.ObjectField(spawner.GetEnemyAt(i), typeof(GameObject), false));
-                spawner.SetCountAt(i, EditorGUILayout.IntField(spawner.GetCountAt(i)));
-                spawner.SetTimeAt(i, EditorGUILayout.FloatField(spawner.GetTimeAt(i)));
-                if (GUILayout.Button("-"))
-                {
-                    spawner.RemoveEntryAt(i);
-                }
-                EditorGUILayout.EndHorizontal();
-            }
-            if (GUILayout.Button("+"))
-            {
-                spawner.AddEntry(null, 1, 0f);
-            }
-            EditorGUILayout.EndVertical();
+            spawned = false;
+
+            if (!float.TryParse(data[0], out time))
+                throw new System.Exception();
+            if (!int.TryParse(data[1], out count))
+                throw new System.Exception();
+            type = data[2];
         }
+
+        public bool spawned;
+        public float time;
+        public int count;
+        public string type;
     }
 }
